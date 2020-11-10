55c81423a780672e0e25ccee6e962aabcd9ab383
Julian Pollinger <neferin42@gmail.com>
2020-11-10 23:21:41 +0100

    Version 1.0

--- index.js
+++ index.js
@@ -4,17 +4,31 @@ import scrapper from "./scrapper.js"
 import sender from "./sender.js";
 dotenv.config();
 
-(async () => {
-    const ret = await scrapper.getData();
-
-    const old = JSON.parse(fs.readFileSync("out.json", {encoding: 'utf8', flag: 'r'}));
-
-    const difference = scrapper.detectChanges(old, ret);
-
-    fs.writeFile("out.json", JSON.stringify(ret), err => {
-        if (err) return console.log(err);
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
     });
-    if (difference.length > 0) {
-        sender.sendChangeNotice(difference);
+}
+
+(async () => {
+    while (true) {
+        const ret = await scrapper.getData();
+
+        if (fs.existsSync("./out.json")) {
+            const old = JSON.parse(fs.readFileSync("out.json", {encoding: 'utf8', flag: 'r'}));
+
+            const difference = scrapper.detectChanges(old, ret);
+
+            if (difference.length > 0) {
+                console.log("Differences found")
+                sender.sendChangeNotice(difference);
+            }
+        }
+
+        fs.writeFile("out.json", JSON.stringify(ret), err => {
+            if (err) return console.log(err);
+        });
+
+        await sleep(60 * 1000);
     }
 })();
