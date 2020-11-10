34919554d0b97c7ab971c37fb4be6511750744c0
Julian Pollinger <neferin42@gmail.com>
2020-11-10 18:56:12 +0100

    Cleanup

--- index.js
+++ index.js
@@ -3,41 +3,10 @@ const puppeteer = require('puppeteer');
 const dotenv = require('dotenv');
 dotenv.config();
 
-function delay(time) {
-    return new Promise(function(resolve) {
-        setTimeout(resolve, time)
-    });
-}
-
-function tableToObject(table) {
-    const data = [];
-
-    // first row needs to be headers
-    const headers = [];
-    for (let i=0; i<table.rows[0].cells.length; i++) {
-        headers[i] = table.rows[0].cells[i].innerHTML.toLowerCase().replace(/ /gi,'');
-    }
-
-    // go through cells
-    for (let i=1; i<table.rows.length; i++) {
-
-        const tableRow = table.rows[i];
-        const rowData = {};
-
-        for (let j=0; j<tableRow.cells.length; j++) {
-
-            rowData[ headers[j] ] = tableRow.cells[j].innerHTML;
-
-        }
-
-        data.push(rowData);
-    }
-
-    return data;
-}
-
 
 async function getObjectsFromTable(page) {
+    const screenshotTable = await page.$('#notenspiegel');
+    await screenshotTable.screenshot({path: 'notenspiegel.png'});
     let table = await page.$$("#notenspiegel tbody tr");
     table.shift();
     let keys = [];
